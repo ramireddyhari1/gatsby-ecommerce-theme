16e9a75c4a19452ad84ce97178bc6bae7b0949cd
ramireddyhari1 <138962813+ramireddyhari1@users.noreply.github.com>
2024-03-07 23:56:43 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
+    title: `SM MAKERS`,
     siteUrl: `https://jamm.matter.design`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Sydney Ecommerce Theme`,
-        short_name: `Sydney`,
+        name: `SM MAKERS`,
+        short_name: `SM`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#ffffff`,
